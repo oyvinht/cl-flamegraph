7e54e99248a2531b4056b731a954c30e7cd82a84
Øyvin Halfdan Thuv <othuv@ravenpack.com>
2023-05-29 21:13:27 +0200

    dunno

--- core.lisp
+++ core.lisp
@@ -1,6 +1,7 @@
 (defpackage #:flamegraph
   (:use #:cl)
   (:import-from #:alexandria)
+  #+sbcl
   (:import-from #:sb-sprof)
   (:nicknames #:flamegraph/core)
   (:export
@@ -15,7 +16,9 @@
   ((func :initarg :func
          :initform nil
          :type (or string
-                   sb-di::compiled-debug-fun)
+                   #+sbcl
+                   sb-di::compiled-debug-fun
+                   null)
          :accessor get-func)
    (counter :initform 0
             :type fixnum
@@ -55,11 +58,22 @@
 (defmethod get-name ((obj string))
   obj)
 
+#+sbcl
 (defmethod get-name ((obj sb-di::compiled-debug-fun))
+  (format t "~%obj: ~a~%name: ~a%" obj (get-name (slot-value obj 'SB-DI::COMPILER-DEBUG-FUN)))
   (get-name (slot-value obj 'SB-DI::COMPILER-DEBUG-FUN)))
 
+#+allegro
+(defmethod get-name ((obj vector))
+  obj)
+
+#+sbcl
 (defmethod get-name ((obj SB-C::COMPILED-DEBUG-FUN))
   (get-name (slot-value obj 'SB-C::NAME)))
+
+#+allegro
+(defmethod get-name ((obj function))
+  (slot-value obj 'symdef))
 
 (defmethod get-name ((obj cons))
   (let ((*print-pretty* nil))
@@ -68,17 +82,29 @@
 (defmethod get-name ((obj symbol))
   (symbol-name obj))
 
+#+sbcl
 (defmethod get-name ((obj sb-kernel:code-component))
   "Some binary code")
 
+#+allegro
+(defmethod get-name ((obj t))
+  obj)
 
+#+sbcl
+(defun aggregate-raw-data ()
+  ;; We need to actually run a report once to make the call graph
+  ;; available to map.
+  (sb-sprof:report :stream (make-broadcast-stream)))
+
+#+sbcl
 (defun make-graph ()
+  (aggregate-raw-data)
   (let ((root (make-instance 'node)))
     (sb-sprof:map-traces
      (lambda (thread trace)
        (declare (ignorable thread))
        (let ((current-node root))
-         (sb-sprof:map-trace-samples
+         (sb-sprof::map-trace-pc-locs
           (lambda (info pc-or-offset)
             (declare (ignorable pc-or-offset))
             (let ((node (search-or-add-child current-node
@@ -93,7 +119,12 @@
 
 
 (defun remove-nodes-up-to-frame (nodes frame)
-  (let ((func (slot-value frame 'sb-di::debug-fun)))
+  (let ((func (slot-value
+               frame
+               #+sbcl
+               'sb-di::debug-fun
+               #+allegro
+               nil)))
     (loop for rest on nodes
           for node = (car rest)
           when (eql (get-func node)
@@ -105,8 +136,11 @@
   (let* ((roots (get-calls root)))
     (labels ((print-path (path count)
                (let* ((nodes (reverse path))
-                      (rest-nodes (remove-nodes-up-to-frame nodes
-                                                            *frame-where-profiling-was-started*))
+                      (rest-nodes #+sbcl
+                                  (remove-nodes-up-to-frame nodes
+                                                            *frame-where-profiling-was-started*)
+                                  #+allegro
+                                  nodes)
                       (names (mapcar #'get-name rest-nodes)))
                  (when names
                    (format stream "~{~A~^;~} ~A~%"
@@ -125,17 +159,37 @@
               roots)
       (values))))
 
-
-(defmacro save-flame-graph ((filename &rest sb-sprof-opts) &body body)
+#+sbcl
+(defmacro save-flame-graph ((filename &rest #+sbcl sb-sprof-opts #+allegro prof-opts) &body body)
   (alexandria:with-gensyms (result-var)
     `(let ((*frame-where-profiling-was-started*
-             (sb-di:top-frame))
+             #+sbcl (sb-di:top-frame)
+             #+allegro nil)
            (,result-var nil))
-       (sb-sprof:with-profiling (,@sb-sprof-opts)
-         (setf ,result-var
-               (multiple-value-list
-                (progn ,@body))))
+       (with-simple-restart (abort "Stop profiling and save graph")
+
+         `(#+sbcl
+           ,@(sb-sprof:with-profiling (,@sb-sprof-opts))
+           #+allegro
+           ,@(prof:with-profiling (,@prof-opts))
+           ,(setf ,result-var
+                  (multiple-value-list
+                   (progn ,@body)))))
        (alexandria:with-output-to-file (s ,filename :if-exists :supersede)
          (print-graph (make-graph)
                       :stream s))
        (values-list ,result-var))))
+
+
+(defun make-graph (tree &optional (count 0))
+  (make-instance 'flamegraph::node
+                 :func (slot-value tree 'prof::entry)
+                 :counter count
+                 :calls (mapcar #'(lambda (c)
+                                    (make-graph c (1+ count)))
+                                (slot-value tree 'prof::children))))
+
+(defun build (saved-profile)
+  (with-slots (prof::actual) saved-profile
+    (with-slots (prof::root-node) prof::actual
+      (make-graph prof::root-node))))
